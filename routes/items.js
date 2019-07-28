a3752f8cca46eeea9848027c015bfbd54f8a0e27
Julia Paulasaari <julia@paulasaari.com>
2019-07-28 01:33:17 +0000

    fixed typo

--- routes/items.js
+++ routes/items.js
@@ -1,6 +1,6 @@
 /*
  * All routes for Users are defined here
- * Since this file is loaded in server.js into api/itemss,
+ * Since this file is loaded in server.js into api/items,
  *   these routes are mounted onto /users
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
