bdda7e6267638bfd9a8c5d17fa02b195a03e4e01
Julia Paulasaari <julia@paulasaari.com>
2019-08-02 17:25:04 +0000

    updated seeds data

---
+++ db/seeds/03_items.sql
@@ -0,0 +1,15 @@
+INSERT INTO items (name, done) 
+VALUES ('Terminator', false),
+('Terminator', true),
+('Gojiro', true),
+('A Place for us', false),
+('A spark of light', false),
+('You think it, I will say it', true),
+('Tap & barrel', false),
+('Boston pizza', true),
+('Gusto di Quattro', false),
+('Interstellar', true),
+('Waiting', true),
+('Skin care set', true),
+('Red bikini', false),
+('iPhone XS', false);
